62246f0d612b5a5644caa356b160e693d8900cd6
Sreejith <abisri024@gmail.com>
2026-08-12 23:08:50 +0530

    clerk login fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,9 @@ import './index.css';
 import App from './App.jsx';
 
 const PUBLISHABLE_KEY =
-import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+  import.meta.env.VITE_CLERK_PUBLISHABLE_KEY ||
+  import.meta.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ||
+  'pk_test_Y2FwaXRhbC10aHJ1c2gtOS5jbGVyay5hY2NvdW50cy5kZXYk';
 
 
 createRoot(document.getElementById('root')).render(
